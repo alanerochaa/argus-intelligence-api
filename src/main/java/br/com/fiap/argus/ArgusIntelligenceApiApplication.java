0605fc5fa8a828a8ae23e9c92974bde21cc7832a
alanerochaa <alanerochaa@gmail.com>
2026-05-28 15:09:19 -0300

    chore: inicializa estrutura base da API Argus Intelligence

---
+++ src/main/java/br/com/fiap/argus/ArgusIntelligenceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.argus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgusIntelligenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgusIntelligenceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/argus/ArgusIntelligenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.argus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgusIntelligenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
