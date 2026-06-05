87c8068bb60a56b747a02eaf32f43bf2851a2fb1
alanerochaa <alanerochaa@gmail.com>
2026-06-05 18:14:06 -0300

    Adicionar autenticação JWT no OpenAPI

--- src/main/java/br/com/fiap/argus/config/OpenApiConfig.java
+++ src/main/java/br/com/fiap/argus/config/OpenApiConfig.java
@@ -1,15 +1,22 @@
 package br.com.fiap.argus.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME =
+            "bearerAuth";
 
     @Bean
     public OpenAPI argusOpenAPI() {
@@ -31,17 +38,32 @@ public class OpenApiConfig {
                                 .description(
                                         """
                                         # Inteligência Ambiental
-    
+                                        
                                         Plataforma responsável pelo monitoramento ambiental,
                                         análise territorial e suporte operacional utilizando dados espaciais.
-    
+                                        
                                         ---
-    
+                                        
                                         ## 🌎 Objetivo
-    
+                                        
                                         Centralizar informações ambientais e apoiar a tomada de decisão
                                         através de monitoramento contínuo e integração entre serviços.
-   
+                                        
+                                        ---
+                                        
+                                        ## 🔐 Segurança
+                                        
+                                        Autenticação centralizada via JWT.
+                                        
+                                        Para testar endpoints protegidos:
+                                        
+                                        1. Clique em **Authorize**
+                                        2. Informe:
+                                        
+                                        Bearer SEU_TOKEN
+                                        
+                                        3. Execute normalmente.
+                                        
                                         """
                                 )
 
@@ -52,6 +74,7 @@ public class OpenApiConfig {
                                                 .name(
                                                         "Grupo CodeGirls"
                                                 )
+
                                 )
 
                                 .license(
@@ -61,8 +84,45 @@ public class OpenApiConfig {
                                                 .name(
                                                         "FIAP • Global Solution 2026/1"
                                                 )
+
+                                )
+
+                )
+
+                .addSecurityItem(
+
+                        new SecurityRequirement()
+
+                                .addList(
+                                        SECURITY_SCHEME
+                                )
+
+                )
+
+                .schemaRequirement(
+
+                        SECURITY_SCHEME,
+
+                        new SecurityScheme()
+
+                                .name(
+                                        "Authorization"
+                                )
+
+                                .type(
+                                        SecurityScheme.Type.HTTP
+                                )
+
+                                .scheme(
+                                        "bearer"
+                                )
+
+                                .bearerFormat(
+                                        "JWT"
                                 )
 
                 );
+
     }
+
 }
